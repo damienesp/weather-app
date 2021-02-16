acf6b55af35ee11102458ce7eb3b945f533022bf
Damien <59603701+damienesp@users.noreply.github.com>
2021-02-16 14:50:42 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,76 @@
+const key = "ee23f572ef86b1ae5a0c728528dff1a3";
+const tempDiv = document.getElementById("temp-card");
+const formDiv = document.querySelector(".form-search");
+const searchInput = document.querySelector(".search-styling");
+
+// fetch data
+async function getWeather(city) {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`);
+  if (!response.ok) {
+    const message = response.status;
+    throw new Error(message);
+  }
+  const data = await response.json();
+
+  addWeather(data);
+}
+
+// add weather in html
+function addWeather(data) {
+  searchInput.classList.remove("error");
+  searchInput.placeholder = "Search by location";
+  const showTemp = document.createElement("div");
+  showTemp.classList.add("weather");
+  showTemp.innerHTML = `<p class="temp-city">${searchInput.value}, ${data.sys.country}</p>
+                        <p class="time-date">${time(data.dt, data.timezone)}</p>
+                        <span class="temp-num">${Math.round(data.main.temp)}°C</span>
+                        <img class="weather-icon" src="./img/weather_icon/${data.weather[0].icon}.svg"/>`;
+  // clean prev temp
+  tempDiv.innerHTML = "";
+  // clean search result
+  searchInput.value = null;
+  // append new temp
+  tempDiv.appendChild(showTemp);
+}
+
+// get time and day by localisation
+function time(dt, tz) {
+  const timedata = new Date((dt + tz) * 1000);
+
+  let hour = timedata.getHours() - 1;
+  let min = timedata.getMinutes();
+  let day = timedata.getDate();
+  let month = timedata.getMonth() + 1;
+
+  if (min < 10) {
+    min = "0" + min;
+  }
+
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+
+  if (month < 10) {
+    month = "0" + month;
+  }
+
+  return `${hour}:${min} • ${day}/${month}`;
+}
+
+// submit event
+formDiv.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const city = searchInput.value;
+
+  getWeather(city).catch((error) => {
+    if (error.message === "404") {
+      searchInput.value = null;
+      searchInput.classList.add("error");
+      searchInput.placeholder = "City not found";
+    } else {
+      searchInput.value = null;
+      searchInput.classList.add("error");
+      searchInput.placeholder = "Server error";
+    }
+  });
+});
