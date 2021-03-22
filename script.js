460e0477029ea055850a7e511be4987f0b1bdf4c
Damien <59603701+damienesp@users.noreply.github.com>
2021-03-22 16:24:33 +0100

    add background and responsive

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const searchInput = document.querySelector(".search-styling");
 
 // fetch data
 async function getWeather(city) {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`);
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`
+  );
   if (!response.ok) {
     const message = response.status;
     throw new Error(message);
@@ -21,10 +23,20 @@ function addWeather(data) {
   searchInput.placeholder = "Search by location";
   const showTemp = document.createElement("div");
   showTemp.classList.add("weather");
-  showTemp.innerHTML = `<p class="temp-city">${searchInput.value}, ${data.sys.country}</p>
-                        <p class="time-date">${time(data.dt, data.timezone)}</p>
-                        <span class="temp-num">${Math.round(data.main.temp)}°C</span>
-                        <img class="weather-icon" src="./img/weather_icon/${data.weather[0].icon}.svg"/>`;
+  showTemp.innerHTML = `<div class="temp-wrapper"><div class="top"><p class="temp-city">${
+    searchInput.value
+  }, ${data.sys.country}</p>
+                        <p class="time-date">${time(
+                          data.dt,
+                          data.timezone
+                        )}</p></div>
+                        <span class="temp-num">${Math.round(
+                          data.main.temp
+                        )}°C</span></div>
+                        <div class="temp-icon">
+                        <img class="weather-icon" src="./img/weather_icon/${
+                          data.weather[0].icon
+                        }.svg"/></div>`;
   // clean prev temp
   tempDiv.innerHTML = "";
   // clean search result
